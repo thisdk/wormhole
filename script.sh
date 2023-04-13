5d621c6c9513de58daeef230953f54ecb9a09ac1
Qixiong Yuan <thisdk@gmail.com>
2023-04-13 15:20:35 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -26,7 +26,7 @@ docker run --restart=always --network host --name kcptun -d kcptun -l 127.0.0.1:
 
 docker run --restart=always --network host --name speederv2 -d speederv2 -s -l127.0.0.1:8388 -r127.0.0.1:1080 -k jason -f2:4 --timeout 0
 
-docker run --restart=always --network host --name udp2raw_kcp --cap-add NET_ADMIN -d udp2raw -s -l0.0.0.0:8585 -r127.0.0.1:8388 -k jason --raw-mode faketcp --cipher-mode none --auth-mode none --keep-rule -a
+docker run --restart=always --network host --name udp2raw_kcp --cap-add NET_ADMIN -d udp2raw -s -l0.0.0.0:8585 -r127.0.0.1:29900 -k jason --raw-mode faketcp --cipher-mode none --auth-mode none --keep-rule -a
 
 docker run --restart=always --network host --name udp2raw_udp --cap-add NET_ADMIN -d udp2raw -s -l0.0.0.0:8686 -r127.0.0.1:8388 -k jason --raw-mode faketcp --cipher-mode none --auth-mode none --keep-rule -a
 
