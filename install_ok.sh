1265c869054831f97920c7146e87cde5835092d9
Qixiong Yuan <thisdk@gmail.com>
2023-05-06 13:17:27 +0800

    Update install_ok.sh

--- install_ok.sh
+++ install_ok.sh
@@ -26,4 +26,4 @@ docker run --restart=always --network host --name udp2raw --cap-add NET_ADMIN -d
 
 docker run --restart=always --network host --name speederv2 -d speederv2 -s -l127.0.0.1:8500 -r127.0.0.1:8400 -k jason -f2:4 --timeout 0
 
-docker run --restart=always --network host --name kcptun -d kcptun -l 127.0.0.1:8400 -t 127.0.0.1:8388 -mode fast3 -nocomp -mtu 256 -crypt none -sndwnd 2048 -rcvwnd 2048 -datashard 2 -parityshard 2 -dscp 46
+docker run --restart=always --network host --name kcptun -d kcptun -l 127.0.0.1:8400 -t 127.0.0.1:8388 -mode fast3 -nocomp -mtu 256 -crypt none -sndwnd 2048 -rcvwnd 2048 -datashard 0 -parityshard 0 -dscp 46
