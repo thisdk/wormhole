006c07a1303392d4e3d8e18cda3542286f4f8c61
Qixiong Yuan <thisdk@gmail.com>
2023-05-08 16:55:06 +0800

    Update install_ok.sh

--- install_ok.sh
+++ install_ok.sh
@@ -4,13 +4,9 @@ mkdir temp && cd temp && mkdir game
 
 wget https://raw.githubusercontent.com/thisdk/wormhole/main/kcptun
 
-wget https://raw.githubusercontent.com/thisdk/wormhole/main/speederv2
-
 wget https://raw.githubusercontent.com/thisdk/wormhole/main/udp2raw
 
 docker build -f kcptun -t kcptun ./game/
-
-docker build -f speederv2 -t speederv2 ./game/
 
 docker build -f udp2raw -t udp2raw ./game/
 
@@ -22,8 +18,6 @@ docker run --restart=always --network bridge --name watchtower -v /var/run/docke
 
 docker run --restart=always --network host --name sing-box -v /etc/sing-box:/etc/sing-box -d ghcr.io/sagernet/sing-box run -c /etc/sing-box/config.json
 
-docker run --restart=always --network host --name udp2raw --cap-add NET_ADMIN -d udp2raw -s -l0.0.0.0:8585 -r127.0.0.1:8500 -k jason --raw-mode faketcp --cipher-mode xor --auth-mode simple -a
+docker run --restart=always --network host --name udp2raw --cap-add NET_ADMIN -d udp2raw -s -l0.0.0.0:8585 -r127.0.0.1:8400 -k jason --raw-mode faketcp --cipher-mode xor --auth-mode simple -a
 
-docker run --restart=always --network host --name speederv2 -d speederv2 -s -l127.0.0.1:8500 -r127.0.0.1:8400 -k jason -f2:4 --timeout 0
-
-docker run --restart=always --network host --name kcptun -d kcptun -l 127.0.0.1:8400 -t 127.0.0.1:8388 -mode fast3 -nocomp -mtu 1200 -crypt none -sndwnd 2048 -rcvwnd 2048 -datashard 0 -parityshard 0 -dscp 46
+docker run --restart=always --network host --name kcptun -d kcptun -l 127.0.0.1:8400 -t 127.0.0.1:8388 -mode fast3 -nocomp -mtu 1350 -crypt none -sndwnd 2048 -rcvwnd 2048 -datashard 2 -parityshard 2 -dscp 46
