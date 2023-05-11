28beca2293f9f6abc16e9e4bd9b406ec7a8033be
Qixiong Yuan <thisdk@gmail.com>
2023-05-11 10:01:10 +0800

    Update install_ok.sh

--- install_ok.sh
+++ install_ok.sh
@@ -18,6 +18,8 @@ docker run --restart=always --network bridge --name watchtower -v /var/run/docke
 
 docker run --restart=always --network host --name sing-box -v /etc/sing-box:/etc/sing-box -d ghcr.io/sagernet/sing-box run -c /etc/sing-box/config.json
 
-docker run --restart=always --network host --name udp2raw --cap-add NET_ADMIN -d udp2raw -s -l0.0.0.0:8585 -r127.0.0.1:8400 -k jason --raw-mode faketcp --cipher-mode aes128cbc --auth-mode md5 -a
+docker run --restart=always --network host --name udp2raw_ipv4 --cap-add NET_ADMIN -d udp2raw -s -l0.0.0.0:8584 -r127.0.0.1:8400 -k jason --raw-mode faketcp --cipher-mode aes128cbc --auth-mode md5 -a
+
+docker run --restart=always --network host --name udp2raw_ipv6 --cap-add NET_ADMIN -d udp2raw -s -l[::]:8586 -r127.0.0.1:8400 -k jason --raw-mode faketcp --cipher-mode aes128cbc --auth-mode md5 -a
 
 docker run --restart=always --network host --name kcptun -d kcptun -l 127.0.0.1:8400 -t 127.0.0.1:8388 -mode fast3 -nocomp -mtu 1350 -crypt none -sndwnd 2048 -rcvwnd 2048 -datashard 2 -parityshard 2 -dscp 46
